ad4894bcc45fd394bac1788538da52e3dfbd9101
Munsterberg <jetrussell93@gmail.com>
2016-09-09 18:19:11 -0400

    add fighter spec for GET /fighters

--- src/routes/api/fighters.js
+++ src/routes/api/fighters.js
@@ -4,7 +4,7 @@ const fightersRouter = Router();
 
 fightersRouter.route('/fighters')
   .get((req, res) => {
-    res.send('/fighters');
+    res.json({id: 1, name: 'Stipe Miocic'});
   });
 
 export default fightersRouter;
--- test/Fighters.spec.js
+++ test/Fighters.spec.js
@@ -4,11 +4,21 @@ import app from '../src/app';
 import {expect} from 'chai';
 import request from 'supertest';
 
-describe('GET all fighters', () => {
-  it('should return all fighters', (done) => {
-    request(app)
-      .get('/api/fighters')
-      .expect(200)
-      .expect('Content-Type', /application\/json/, done)
-  });
+describe('Fighters spec', () => {
+  describe('GET all fighters', () => {
+    it('should return all fighters', (done) => {
+      request(app)
+        .get('/api/fighters')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        .end((err, res) => {
+          expect(res.body.id).to.not.equal(null);
+          expect(res.body).to.have.property('name');
+          expect(res.body.name).to.not.equal(null);
+          expect(res.body.id).to.be.above(0);
+          done();
+        });
+    });
+  })
 });
